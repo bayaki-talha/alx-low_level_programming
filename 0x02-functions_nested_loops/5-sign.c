458d4da9d3d5435a07e9615b0d516ebb582582f6
bayaki-talha <bayakitalha9@gmail.com>
2023-10-10 12:35:56 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,7 +6,7 @@
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
- */
+*/
 int print_sign(int n)
 {
 	if (n > 0)
@@ -14,10 +14,10 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	} else if (n == 0)
-}
+
 		_putchar(48);
 		return (0);
-	} else if (n < 0)
+else if (n < 0)
 	{
 		_putchar('-');
 	}
