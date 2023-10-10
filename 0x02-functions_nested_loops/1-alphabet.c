fe5a4f921cc5e8c8c63673c5119e4d98c8bd1478
bayaki-talha <bayakitalha9@gmail.com>
2023-10-10 09:31:48 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
- * print_alphabet - utilizes on the _putchar function to print the alphabet a - z
+ * print_alphabet - utilizes on the _putchar function to print alphabet a - z
  *
 */
 
