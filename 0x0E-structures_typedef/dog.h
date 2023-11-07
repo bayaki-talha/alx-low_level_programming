083cd006bfcca89793bc5fb08a092d1ace3fb8fd
bayaki-talha <bayakitalha9@gmail.com>
2023-11-06 18:48:47 -0800

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,8 @@ struct dog
 
 typedef struct dog dog_t;
 
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
