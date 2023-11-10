573223dbdbb23846d0af1a4281c140080e8e6d57
bayaki-talha <bayakitalha9@gmail.com>
2023-11-10 20:31:53 +0400

    a

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints name use pointer function
+ * @name: string
+ * @f: function pointer
+ * Return: 0
+*/
+void print_name(char*name, void(*f)(char*))
+{
+	if (name == NULL || f == NULL)
+		return 0;
+	f(name);
+}
